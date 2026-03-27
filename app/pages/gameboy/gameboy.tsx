9b1621416d50f24dd21147891707f8ac3b97919a
bytesizedfox <bytesizedfox@gmail.com>
2026-03-27 18:26:53 -0400

    add basic controls list and download button for GBA rom

--- app/pages/gameboy/gameboy.tsx
+++ app/pages/gameboy/gameboy.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DecDeBox } from "~/components/containers/DecDeSection";
 import { DecDeWidthContainer } from "~/components/containers/DecDeWidthContainer";
 import { SiteFooter } from "~/components/site/SiteFooter";
@@ -10,12 +10,15 @@ const GameBoyPage = () => {
   const ref = useRef<HTMLCanvasElement>(null);
   const gameboy = useGameBoy(ref);
 
+  const [downloadURL, setDownloadURL] = useState("");
+
   useEffect(() => {
     if (gameboy) {
       const load = async () => {
         const req = await fetch(dectalk);
         const blob = await req.blob();
         const file = new File([blob], "dectalk.gba");
+        setDownloadURL(URL.createObjectURL(file));
 
         gameboy.uploadRom(file, () => {
           gameboy.loadGame(gameboy.filePaths().gamePath + "/dectalk.gba");
@@ -31,8 +34,18 @@ const GameBoyPage = () => {
         <DecDeWidthContainer>
           <SiteHeader />
           <DecDeBox>
-            <canvas ref={ref} />
-          </DecDeBox>
+            <h1 style={{width: "100%", textAlign: "center"}}>Gameboy advance demo!</h1>
+            <div style={{margin: "auto", width: "300px"}}>
+                <canvas ref={ref} />
+            </div>
+            <a href={downloadURL} >Download ROM here!</a>
+            <h3>Controls:</h3>
+            <span>Arrow keys -&gt; D-Pad</span><br></br>
+            <span>x -&gt; A</span><br></br>
+            <span>backspace -&gt; SELECT</span><br></br>
+            <span>enter -&gt; START</span><br></br>
+
+            </DecDeBox>
           <SiteFooter />
         </DecDeWidthContainer>
       </main>
