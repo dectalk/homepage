95fe12fd609a6ab470addc4dd891748a514cf309
Leondro Lio <github@leondrolio.com>
2026-03-28 01:18:45 +0000

    prerender gameboy

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   prerender: {
-    paths: ["/"]
-  }
+    paths: ["/", "/gameboy"],
+  },
 } satisfies Config;
