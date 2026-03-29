b45263d2996ebfe5b8d370e0eaa69a2ba759abd7
Leondro Lio <github@leondrolio.com>
2026-03-29 19:57:20 +0100

    pregenerate webspeak page

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   prerender: {
-    paths: ["/", "/gameboy", "/404"],
+    paths: ["/", "/gameboy", "/404", "/webspeak"],
   },
   ssr: false,
 } satisfies Config;
