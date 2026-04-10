b86c8d27d67a344e1fdf8a38f31d842b0655cc29
bytesizedfox <bytesizedfox@gmail.com>
2026-04-09 20:44:13 -0400

    revert header changes and try using plugin-legacy for archival support due to ES module incompatabilites with pages like web archive

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
     reactRouter(),
     tsconfigPaths(),
+    {
+      name: "configure-response-headers",
+      configureServer: server => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
   ],
   assetsInclude: ["**/*.gba"],
   optimizeDeps: {
